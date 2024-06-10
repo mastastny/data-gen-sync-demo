7ada9fd5d335e00c2e9a83816c62a501a8b240c1
Marek Stastny <mstastny@redhat.com>
2024-06-10 18:43:00 +0200

    Generate basic structure

---
+++ src/main/java/io/patriot_framework/data_gen_sync_demo/App.java
@@ -0,0 +1,13 @@
+package io.patriot_framework.data_gen_sync_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/io/patriot_framework/data_gen_sync_demo/AppTest.java
@@ -0,0 +1,20 @@
+package io.patriot_framework.data_gen_sync_demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
